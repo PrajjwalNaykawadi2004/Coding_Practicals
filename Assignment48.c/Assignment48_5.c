2a25644618dac96636f75fef61bcd77b7d704815
Prajjwal Naykawadi <naykawadiprajjwal@gmail.com>
2026-02-03 11:40:08 +0530

    Add files via upload

---
+++ Assignment48.c/Assignment48_5.c
@@ -0,0 +1,511 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+struct node
+{
+    int Data;
+    struct node *Next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name    :   InsertFirst
+// Description      :   Insert node at beginning
+// Input            :   PPNODE, int
+// Output           :   void
+// Author           :   Prajjwal Dilip Naykawadi
+// Date             :   03/02/2026
+// 
+//////////////////////////////////////////////////////////////////////////////////////
+
+void InsertFirst(PPNODE Head, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->Data = no;
+    newn->Next = NULL;
+
+    if(*Head == NULL)
+    {
+        (*Head) = newn;
+    }
+    else
+    {
+        newn->Next = *Head;
+        (*Head) = newn;
+    }  
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name    :   InsertLast
+// Description      :   Insert node at end
+// Input            :   PPNODE, int
+// Output           :   void
+// Author           :   Prajjwal Dilip Naykawadi
+// Date             :   03/02/2026
+// 
+//////////////////////////////////////////////////////////////////////////////////////
+
+void InsertLast(PPNODE Head, int no)
+{
+    PNODE temp = NULL;
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->Data = no;
+    newn->Next = NULL;
+
+    if(*Head == NULL)
+    {
+        (*Head) = newn;
+    }
+    else
+    {
+        temp = *Head;
+
+        while(temp->Next != NULL)
+        {
+            temp = temp->Next;
+        }
+        temp->Next = newn;
+    }  
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name    :   DeleteFirst
+// Description      :   Delete first node
+// Input            :   PPNODE
+// Output           :   void
+// Author           :   Prajjwal Dilip Naykawadi
+// Date             :   03/02/2026
+// 
+//////////////////////////////////////////////////////////////////////////////////////
+
+void DeleteFirst(PPNODE Head)
+{
+    PNODE temp = NULL;
+
+    if((*Head) == NULL)
+    {
+        return;
+    }
+    else if((*Head)->Next == NULL)
+    {
+        free(*Head);
+        *Head = NULL;
+    }
+    else
+    {
+        temp = *Head;
+        *Head = (*Head)->Next;
+        free(temp);
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name    :   DeleteLast
+// Description      :   Delete last node
+// Input            :   PPNODE, int
+// Output           :   void
+// Author           :   Prajjwal Dilip Naykawadi
+// Date             :   03/02/2026
+// 
+//////////////////////////////////////////////////////////////////////////////////////
+
+void DeleteLast(PPNODE Head)
+{
+    PNODE temp = NULL;
+
+    if((*Head) == NULL)
+    {
+        return;
+    }
+    else if((*Head)->Next == NULL)
+    {
+        free(*Head);
+        *Head = NULL;
+    }
+    else
+    {
+        temp = *Head;
+
+        while(temp->Next->Next != NULL)
+        {
+            temp = temp->Next;
+        }
+
+        free(temp->Next);
+        temp->Next = NULL;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name    :   Display
+// Description      :   Display Linked List of all functions
+// Input            :   PPNODE
+// Output           :   void
+// Author           :   Prajjwal Dilip Naykawadi
+// Date             :   03/02/2026
+// 
+//////////////////////////////////////////////////////////////////////////////////////
+
+void Display(PNODE Head)
+{
+    while(Head != NULL)
+    {
+        printf("| %d |->",Head->Data);
+        Head = Head->Next;
+    }
+    printf("NULL\n");
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name    :   Count
+// Description      :   Count number of nodes
+// Input            :   PPNODE
+// Output           :   void
+// Author           :   Prajjwal Dilip Naykawadi
+// Date             :   03/02/2026
+// 
+//////////////////////////////////////////////////////////////////////////////////////
+
+int Count(PNODE Head)
+{
+    int iCount = 0;
+
+    while(Head != NULL)
+    {
+        iCount++;
+        Head = Head->Next;
+    }
+    return iCount;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name    :   InsertAtPos
+// Description      :   Insert node at given position
+// Input            :   PPNODE
+// Output           :   void
+// Author           :   Prajjwal Dilip Naykawadi
+// Date             :   03/02/2026
+// 
+//////////////////////////////////////////////////////////////////////////////////////
+
+void InsertAtPos(PPNODE Head, int no, int pos)
+{
+    PNODE temp = NULL;
+    PNODE newn = NULL;
+
+    int iCnt = 0;
+    int iSize = 0;
+    iSize = Count(*Head);
+
+    if((pos < 1 ) || (pos > iSize + 1) )          
+    {
+        printf("Invalid position\n");
+        return;
+    }
+
+    if(pos == 1)
+    {
+        InsertFirst(Head,no);
+    }
+    else if(pos == iSize + 1)
+    {
+        InsertLast(Head,no);
+    }
+    else
+    {
+        newn = (PNODE)malloc(sizeof(NODE));
+
+        newn->Data = no;
+        newn->Next = NULL;
+
+        temp = *Head;
+
+        for(iCnt = 1; iCnt < pos-1; iCnt++)
+        {
+            temp = temp ->Next;
+        }
+
+        newn->Next = temp ->Next;  
+        temp->Next = newn;         
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name    :   DeleteAtPos
+// Description      :   Delete node at given position
+// Input            :   PPNODE, int
+// Output           :   void
+// Author           :   Prajjwal Dilip Naykawadi
+// Date             :   03/02/2026
+// 
+//////////////////////////////////////////////////////////////////////////////////////
+
+void DeleteAtPos(PPNODE Head, int pos)
+{
+    int iSize = 0;
+    int iCnt = 0;
+
+    PNODE temp = NULL;
+    PNODE target = NULL;
+    
+    iSize = Count(*Head);
+
+    if((pos < 1 ) || (pos > iSize))          
+    {
+        printf("Invalid position\n");
+        return;
+    }
+
+    if(pos == 1)
+    {
+        DeleteFirst(Head);
+    }
+    else if(pos == iSize)
+    {
+        DeleteLast(Head);
+    }
+    else
+    {
+        temp = *Head;
+
+        for(iCnt = 1; iCnt < pos - 1; iCnt++)
+        {
+            temp = temp->Next;
+        }
+
+        target = temp->Next;
+        temp->Next = target->Next;
+        free(target);
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name    :   ReplaceEven
+// Description      :   Replace all even elements with 0
+// Input            :   PPNODE
+// Output           :   void
+// Author           :   Prajjwal Dilip Naykawadi
+// Date             :   03/02/2026
+// 
+//////////////////////////////////////////////////////////////////////////////////////
+
+void ReplaceEven(PPNODE Head)
+{
+    PNODE temp = *Head;
+
+    while(temp != NULL)
+    {
+        if((temp->Data % 2) == 0)
+        {
+            temp->Data = 0;
+        }
+        temp = temp->Next;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name    :   ReplaceOdd
+// Description      :   Replace all odd elements with 1
+// Input            :   PPNODE
+// Output           :   void
+// Author           :   Prajjwal Dilip Naykawadi
+// Date             :   03/02/2026
+// 
+//////////////////////////////////////////////////////////////////////////////////////
+
+void ReplaceOdd(PPNODE Head)
+{
+    PNODE temp = *Head;
+
+    while(temp != NULL)
+    {
+        if((temp->Data % 2) != 0)
+        {
+            temp->Data = 1;
+        }
+        temp = temp->Next;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name    :   MultiplyByTwo
+// Description      :   Multiply each element of linked list by 2
+// Input            :   PPNODE
+// Output           :   void
+// Author           :   Prajjwal Dilip Naykawadi
+// Date             :   03/02/2026
+// 
+//////////////////////////////////////////////////////////////////////////////////////
+
+void MultiplyByTwo(PPNODE Head)
+{
+    PNODE temp = *Head;
+
+    while(temp != NULL)
+    {
+        temp->Data = temp->Data * 2;
+        temp = temp->Next;
+    }
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name    :   DisplayEvenPosition
+// Description      :   Display elements present at even position
+// Input            :   PNODE
+// Output           :   void
+// Author           :   Prajjwal Dilip Naykawadi
+// Date             :   03/02/2026
+// 
+//////////////////////////////////////////////////////////////////////////////////////
+
+void DisplayEvenPosition(PNODE Head)
+{
+    int iPos = 1;
+
+    while(Head != NULL)
+    {
+        if((iPos % 2) == 0)
+        {
+            printf("%d\t", Head->Data);
+        }
+        iPos++;
+        Head = Head->Next;
+    }
+    printf("\n");
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name    :   DisplayOddPosition
+// Description      :   Display elements present at odd position
+// Input            :   PNODE
+// Output           :   void
+// Author           :   Prajjwal Dilip Naykawadi
+// Date             :   03/02/2026
+// 
+//////////////////////////////////////////////////////////////////////////////////////
+
+void DisplayOddPosition(PNODE Head)
+{
+    int iPos = 1;
+
+    while(Head != NULL)
+    {
+        if((iPos % 2) != 0)
+        {
+            printf("%d\t", Head->Data);
+        }
+        iPos++;
+        Head = Head->Next;
+    }
+    printf("\n");
+}
+
+
+int main()
+{
+    PNODE First = NULL;
+    int iRet = 0;
+    BOOL bRet = FALSE;
+
+    InsertFirst(&First, 101);
+    InsertFirst(&First, 40);
+    InsertFirst(&First, 20);
+    InsertFirst(&First, 10);
+
+    Display(First);
+    iRet = Count(First);
+    printf("Number of nodes are : %d\n", iRet);
+
+    InsertLast(&First, 101);
+    InsertLast(&First, 111);
+    InsertLast(&First, 12);
+
+    Display(First);
+    iRet = Count(First);
+    printf("Number of nodes are : %d\n", iRet);
+
+    DeleteFirst(&First);
+
+    Display(First);
+    iRet = Count(First);
+    printf("Number of nodes are : %d\n", iRet);
+
+    DeleteLast(&First);
+
+    Display(First);
+    iRet = Count(First);
+    printf("Number of nodes are : %d\n", iRet);
+
+    InsertAtPos(&First,10,3);
+
+    Display(First);
+    iRet = Count(First);
+    printf("Number of nodes are : %d\n", iRet);
+
+    DeleteAtPos(&First,3);
+
+    Display(First);
+    iRet = Count(First);
+    printf("Number of nodes are : %d\n", iRet);
+
+    printf("Before replacing even elements:\n");
+    Display(First);
+
+    ReplaceEven(&First);
+
+    printf("After replacing even elements:\n");
+    Display(First);
+
+    printf("Before replacing odd elements:\n");
+    Display(First);
+
+    ReplaceOdd(&First);
+
+    printf("After replacing odd elements:\n");
+    Display(First);
+
+    printf("Before multiplying elements by 2:\n");
+    Display(First);
+
+    MultiplyByTwo(&First);
+
+    printf("After multiplying elements by 2:\n");
+    Display(First);
+
+    printf("Elements at even positions are:\n");
+    DisplayEvenPosition(First);
+
+    printf("Elements at odd positions are:\n");
+    DisplayOddPosition(First);
+
+    return 0;
+}
